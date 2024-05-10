cf3a5557f6c51df5f25b6b7947c4dfb5c6bb4437
Zidaan16 <ahmadzidan1316@gmail.com>
2024-05-11 00:59:36 +0700

    First commit

---
+++ Attributes/AbstractRouter.php
@@ -0,0 +1,6 @@
+<?php
+namespace Tiaras\Attributes;
+
+abstract class AbstractRouter {
+    //
+}
---
+++ src/Route.php
@@ -0,0 +1,12 @@
+<?php
+namespace Tiaras\Services;
+use Tiaras\Attributes\AbstractRouter;
+
+class Route extends AbstractRouter {
+
+    public function __construct()
+    {
+        
+    }
+
+}
